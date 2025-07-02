69c57b1380c538ce3d1d7d5b481f504302798d01
piyushayu <piyush.ps1412@outlook.com>
2025-07-02 11:20:42 +0000

    final change

---
+++ js.hindi/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =12345
+let accountEmail="piuus@gmail.com"
+var accountpassword="6758"
+accountcity="jaipur"
+
+//accountId ="dhdgieo"
+//not allowed to change id
+
+
+
+
+
+
+
+
+//console.log(accountId);
+
+console.log(accountId, accountEmail, accountpassword, accountcity);
+

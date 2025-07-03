ff537c7c7d46e45da8bbd9461c70b3ce48f8b384
piyushayu <piyush.ps1412@outlook.com>
2025-07-03 05:47:01 +0000

    convert and operation

--- js.hindi/01_variables.js
+++ js.hindi/01_variables.js
@@ -3,13 +3,20 @@ let accountEmail="piuus@gmail.com"
 var accountpassword="6758"
 accountcity="jaipur"
 
+let accountState;
+//bina value ke variable declare karne par undefined milega//
+
 //accountId ="dhdgieo"
 //not allowed to change id
 
+accountEmail=" hdd.com"
+accountpassword="hddhhd"
+accountcity="bihar"
 
-
-
-
+/*
+kabhi var use mat karna
+use let or const
+*/
 
 
 
